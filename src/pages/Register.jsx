2e48cf02076f9fcc05ca3d1afafc2c63e83b5927
Web Real <webrealcoding@gmail.com>
2023-02-25 01:06:52 +0800

    add: error message in register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,68 +1,79 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useRef, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ValidateEmail from '../components/ValidateEmail';
 import { GlobalContext } from '../components/GlobalContext';
 
 const Register = () => {
+  const initialValue = {
+    id: Number(new Date()),
+    fullName: '',
+    email: '',
+    password: '',
+    isLoggedIn: false,
+  };
+
   const { users } = useContext(GlobalContext);
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [userValues, setUserValues] = useState(initialValue);
+  const [formErrors, setFormErrors] = useState({});
+  const confirmPassword = useRef();
   const navigate = useNavigate();
+
+  const emails = useMemo(() => {
+    return users.map((user) => user.email);
+  }, [users]);
+
+  // returns error message
+  const validation = () => {
+    const errors = {};
+    if (!userValues.fullName) {
+      errors.fullName = 'Please enter your full name';
+    }
+
+    if (emails.includes(userValues.email)) {
+      errors.emailExist = 'Sorry, Email already exist';
+    }
+
+    if (!ValidateEmail(userValues.email)) {
+      errors.invalidEmail = 'Invalid Email';
+    }
+
+    if (userValues.password.length <= 7 || !userValues.password) {
+      errors.password = 'Password length must be 8 characters';
+    }
+
+    if (confirmPassword.current.value !== userValues.password) {
+      errors.confirmPassword = 'Password not match';
+    }
+    setFormErrors(errors);
+    return errors;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const errors = validation();
 
-    const emails = [];
-    users.map((user) => {
-      emails.push(user.email);
-    });
-
-    if (!fullName) {
-      alert('Please enter your full name');
-      return false;
-    }
-
-    if (emails.includes(email)) {
-      alert('Email already exist');
-      return false;
-    }
-
-    if (!ValidateEmail(email)) {
-      alert('Invalid Email');
-      return false;
-    }
-
-    if (password.length <= 7 || !password) {
-      alert('Password length must be 8 characters');
-      return false;
-    }
-
-    if (confirmPassword !== password) {
-      alert('Password not match');
-      return false;
+    if (Object.keys(errors).length !== 0) {
+      return;
     }
 
     // registration user object
-    const usersCopy = users;
-    const user = {
-      id: Number(new Date()),
-      fullName: fullName,
-      email: email,
-      password: password,
+    const newUser = {
+      id: userValues.id,
+      fullName: userValues.fullName,
+      email: userValues.email,
+      password: userValues.password,
       isLoggedIn: false,
     };
-    usersCopy.push(user);
+    users.push(newUser);
     // update the localStorage
-    localStorage.setItem('users', JSON.stringify(usersCopy));
+    localStorage.setItem('users', JSON.stringify(users));
     navigate('/registersuccessful');
   };
 
   return (
     <div className="section-container">
       <h1>Register</h1>
-      <form className="default-form" onSubmit={handleSubmit}>
+      <form className="default-form register-from" onSubmit={handleSubmit}>
         <div>
           <label>Full Name</label>
           <input
@@ -71,9 +82,12 @@ const Register = () => {
             type="text"
             name="fullName"
             placeholder="Name"
-            onChange={(e) => setFullName(e.target.value)}
+            onChange={(e) =>
+              setUserValues({ ...userValues, fullName: e.target.value })
+            }
           />
         </div>
+        <p className="error-message-registration">{formErrors.fullName}</p>
         <div>
           <label>Email</label>
           <input
@@ -82,9 +96,14 @@ const Register = () => {
             type="mail"
             placeholder="Email"
             name="email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) =>
+              setUserValues({ ...userValues, email: e.target.value })
+            }
           />
         </div>
+        <p className="error-message-registration">
+          {formErrors.emailExist || formErrors.invalidEmail}
+        </p>
         <div>
           <label>Password</label>
           <input
@@ -93,9 +112,12 @@ const Register = () => {
             type="password"
             placeholder="Password"
             name="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) =>
+              setUserValues({ ...userValues, password: e.target.value })
+            }
           />
         </div>
+        <p className="error-message-registration">{formErrors.password}</p>
         <div>
           <label>Comfirm Password</label>
           <input
@@ -104,9 +126,12 @@ const Register = () => {
             type="password"
             placeholder="Confirm Password"
             name="confirmPassword"
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            ref={confirmPassword}
           />
         </div>
+        <p className="error-message-registration">
+          {formErrors.confirmPassword}
+        </p>
         <div>
           <input
             className="form-button register-submit-button"

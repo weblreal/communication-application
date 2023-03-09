c3f1685b77687d668b37196fcb2a1bcfdb5d602d
Web Real <webrealcoding@gmail.com>
2023-03-09 14:36:50 +0800

    organize code structure

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,35 +1,35 @@
 import React, { useState, useEffect, createContext } from 'react';
-
 export const GlobalContext = createContext();
 
-export default function GlobalContextProvider({ children }) {
-  // get item in the localstorage
-  const getItem = (itemName) => {
-    return localStorage.getItem(itemName)
-      ? JSON.parse(localStorage.getItem(itemName))
-      : [];
-  };
+// Helper: get item in the local storage
+const getItem = (itemName) => {
+  return localStorage.getItem(itemName)
+    ? JSON.parse(localStorage.getItem(itemName))
+    : [];
+};
 
+export default function GlobalContextProvider({ children }) {
   const [users, setUsers] = useState(getItem('users'));
-  const [loggedIn, setLoggedIn] = useState(getItem('loggedIn'));
   const [chats, setChats] = useState(getItem('chats'));
+  const [loggedIn, setLoggedIn] = useState(getItem('loggedIn'));
   const [myUploads, setMyUploads] = useState(getItem('myUploads'));
 
+  // Updates the local storage
   useEffect(() => {
     localStorage.setItem('users', JSON.stringify(users));
-    localStorage.setItem('loggedIn', JSON.stringify(loggedIn));
     localStorage.setItem('chats', JSON.stringify(chats));
+    localStorage.setItem('loggedIn', JSON.stringify(loggedIn));
     localStorage.setItem('myUploads', JSON.stringify(myUploads));
   }, [chats, users, loggedIn, myUploads]);
 
   const values = {
     setUsers,
-    setMyUploads,
     setChats,
     setLoggedIn,
-    loggedIn,
+    setMyUploads,
     users,
     chats,
+    loggedIn,
     myUploads,
   };
 
